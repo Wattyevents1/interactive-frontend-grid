c15110450860e1a652536de41a55aa562e8a1eb1
Wattyevents1 <wattysamuel@gmail.com>
2025-07-19 20:46:07 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === 'production' ? '/your-repo-name/' : '/',
+  base: mode === 'production' ? '/portfolio-watty/' : '/',
   plugins: [
     react(),
     mode === 'development' &&
